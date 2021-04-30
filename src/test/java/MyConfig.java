b1af6303fa310c09726a2ac60272a661fbbd2ad0
Ian Flanagan <ianianf@yahoo.com>
2021-04-30 15:47:47 -0500

    removed tokens and other stuff.

--- src/test/java/MyConfig.java
+++ src/test/java/MyConfig.java
@@ -4,7 +4,7 @@ public class MyConfig {
 
     public static String getMyBuildFile = "build.txt";
     public static final String automationToken = "<token>";
-    public static final String myToken = "404e3cc8d178433693af2f4c051b9fa6"; //   use <post_server_item> token for your rollbar project
+    public static final String myToken = "<yourToken>"; //   use <post_server_item> token for your rollbar project
     public static final String myENv =  Utils.changeEnv(); // "prod";
     public static double myVersion;
 
@@ -22,8 +22,8 @@ public class MyConfig {
     public static final String platform = "macOS";
 
     // Server Provider info
-    public static final String myRoot = "com.IanFlanagan";
-    public static final String myHost = "Ians-MacBook-Pro.local"; // old localhost new Utils.GetHostName(); Ians-MacBook-Pro.local
+    public static final String myRoot = "JavaJenkinsNew";
+    public static final String myHost = "MacBook-Pro.local"; // old localhost new Utils.GetHostName();
     public static final String myBranch = "main";
 
     // other config stuff
